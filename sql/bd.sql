0d08d9605a30db750daf0d487cdb16d0d364d7fe
asix2pj9 <asix2pj9@proton.me>
2026-01-21 11:07:45 +0100

    Commit 3 de l'aplicació sm8act04

--- sql/bd.sql
+++ sql/bd.sql
@@ -1,14 +1,16 @@
-use xxxxxxx;
-create table xxxxxx(
-	codi int(10) unsigned auto_increment not null,
-	nom varchar(20) not null,
-	cognoms varchar(50) not null,
-	email varchar(50) not null,
-    primary key (codi)
-);
-insert into xxxxxx (nom,cognoms,email) values("paula", "alonso pons", "app@clot.com");
-insert into xxxxxx (nom,cognoms,email) values("joan", "pelayo rocamora", "jrr@fje.net");
-insert into xxxxxx (nom,cognoms,email) values("pere", "masponts matadepera", "pmm@daw2.com");
-insert into xxxxxx (nom,cognoms,email) values("esther", "casajoana contreras", "mcc@fjeclot.com");
-insert into xxxxxx (nom,cognoms,email) values("sergi", "dalmau delacroix", "jdd@clotfje.net");
+USE bdEmpresa;
 
+CREATE TABLE Treballadors (
+	codi int UNSIGNED AUTO_INCREMENT,
+	nom VARCHAR(50) NOT NULL,
+	cognoms VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (codi)
+);
+
+INSERT INTO Treballadors (nom,cognoms,email) VALUES
+('paula', 'alonso pons', 'app@clot.com'),
+('joan', 'pelayo rocamora', 'jrr@fje.net'),
+('pere', 'masponts matadepera', 'pmm@daw2.com'),
+('esther', 'casajoana contreras', 'mcc@fjeclot.com'),
+('sergi', 'dalmau delacroix', 'jdd@clotfje.net');
